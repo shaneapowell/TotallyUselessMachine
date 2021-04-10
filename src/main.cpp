1d97f6887865732068215850ead380979c88e264
shaneapowell <shaneapowell@hotmail.com>
2021-04-10 17:17:47 -0500

    Added 16 more sequences.

--- src/main.cpp
+++ src/main.cpp
@@ -17,11 +17,11 @@
 #define TRIGGER_PIN 	2
 #define RELAY_PIN 		11
 
-#define ARM_HOME  120
-#define ARM_FULL  8
+#define DOOR_CLOSED 60
+#define DOOR_OPEN 155
 
-#define DOOR_CLOSED 150
-#define DOOR_OPEN 60
+#define ARM_HOME  10
+#define ARM_FULL  122
 
 #define SERVO_FAST  	1
 #define SERVO_SLOW  	70
@@ -130,18 +130,14 @@ void parkServos()
 }
 
 
-// Sequences from "Moody Useless Machine" by Lamja Electronics
-// http://www.lamja.com/?p=451
-// http://www.lamja.com/blogfiles/UselessMachine.pde
-
 /************************************************/
 void sequence1()
 {
 	doorTo(40,  60,   1000);
 	doorTo(0,   100, 1000);
 	doorTo(100, 100,  50);
-	 armTo(100, 100, 300);
-	 armTo(0,   100, 100);
+	armTo(100, 100, 300);
+	armTo(0,   100, 100);
 	doorTo(0,   100, 400);
 }
 
@@ -362,6 +358,354 @@ void sequence14()
 	doorTo(0, 100, 1);
 }
 
+/************************************************/
+void sequence15()
+{
+	doorTo(100, 100, 1);
+	armTo(100, 100, 500);
+	armTo(50, 90, 100);
+	armTo(100, 100, 500);
+	armTo(50, 90, 100);
+	armTo(100, 100, 500);
+	armTo(50, 90, 500);
+	armTo(100, 100, 500);
+	armTo(50, 90, 100);
+	armTo(100, 100, 500);
+	armTo(50, 90, 100);
+	armTo(100, 100, 500);
+	armTo(50, 90, 1000);
+	armTo(100, 100, 500);
+	armTo(50, 90, 500);
+	armTo(100, 100, 500);
+	armTo(50, 90, 100);
+	armTo(100, 100, 500);
+	armTo(50, 90, 100);
+	armTo(100, 100, 500);
+	armTo(0, 100, 100);
+	doorTo(0, 100, 1);
+}
+
+/************************************************/
+void sequence16()
+{
+	doorTo(50, 90, 1000);
+	doorTo(0,  90, 1000);
+	doorTo(50, 90,  500);
+	doorTo(0,  90, 2000);
+	doorTo(50, 90, 1500);
+	doorTo(0,  60, 1000);
+	doorTo(60, 90, 1200);
+	doorTo(0,  40, 1000);
+	doorTo(50, 80, 1000);
+	doorTo(60, 80, 1000);
+	doorTo(70, 80, 3000);
+	doorTo(100,100, 1);
+
+	armTo(100, 100, 500);
+	armTo(0, 100, 100);
+	doorTo(0, 100, 1);
+}
+
+/************************************************/
+void sequence17()
+{
+	doorTo(50, 90, 800);
+	doorTo(10, 90, 100);
+	doorTo(60, 90, 900);
+	doorTo(10, 90, 100);
+	doorTo(60, 90, 1200);
+	doorTo(10, 90, 100);
+	doorTo(80, 90, 1200);
+	doorTo(90, 90, 500);
+	doorTo(100,90, 800);
+	doorTo(90, 90, 900);
+	doorTo(80, 80, 1100);
+	doorTo(70, 80, 900);
+	doorTo(60, 80, 700);
+	doorTo(100, 90, 300);
+	doorTo(30, 40,  100);
+	doorTo(100, 95, 300);
+
+	armTo(100, 100, 200);
+	armTo(0, 100, 100);
+
+	doorTo(0, 100, 1);
+}
+
+/************************************************/
+void sequence18()
+{
+	doorTo(100, 70, 100);
+	armTo(80, 100, 3500);
+
+	armTo(100, 100, 100);
+	armTo(0, 100, 100);
+	doorTo(0, 100, 1);
+}
+
+/************************************************/
+void sequence19()
+{
+	doorTo(50,  90, 200);
+	armTo(20,  100, 200);
+	doorTo(60, 100, 200);
+	armTo(40,  100, 200);
+	doorTo(70, 100, 200);
+	armTo(60,  100, 200);
+	doorTo(80, 100, 200);
+	armTo(80,  100, 200);
+	doorTo(90, 100, 200);
+	armTo(90,  100, 200);
+	doorTo(100, 100, 200);
+	armTo(100,  100, 200);
+
+	armTo(90,  100, 200);
+	doorTo(90, 100, 200);
+	armTo(80,  100, 200);
+	doorTo(80, 100, 200);
+	armTo(60,  100, 200);
+	doorTo(70, 100, 200);
+	armTo(40,  100, 200);
+	doorTo(60, 100, 200);
+	armTo(20,  100, 200);
+	doorTo(50, 100, 200);
+
+	armTo(0,  100, 200);
+	doorTo(0, 100, 1);
+}
+
+/************************************************/
+void sequence20()
+{
+	doorTo(100, 90, 100);
+
+	armTo(10, 100, 10);
+	armTo(15, 95, 10);
+	armTo(20, 90, 10);
+	armTo(25, 85, 10);
+	armTo(30, 80, 10);
+	armTo(35, 75, 10);
+	armTo(40, 70, 10);
+	armTo(45, 65, 10);
+	armTo(50, 60, 10);
+	armTo(55, 55, 10);
+	armTo(60, 50, 10);
+	armTo(65, 45, 10);
+	armTo(70, 40, 10);
+	armTo(75, 35, 10);
+	armTo(80, 30, 10);
+	armTo(85, 25, 10);
+	armTo(90, 30, 10);
+	armTo(95, 15, 10);
+	armTo(100,10, 500);
+		
+	armTo(0, 100, 100);
+	doorTo(0, 100, 1);
+}
+
+/************************************************/
+void sequence21()
+{
+	doorTo(10,  100, 8000);
+	doorTo(100, 99,  100);
+	armTo(100, 100, 500);
+	armTo(0, 100, 100);
+	doorTo(0, 100, 1);
+}
+
+/************************************************/
+void sequence22()
+{
+	doorTo(100,  95, 100);
+	armTo(80, 100, 500);
+	armTo(0, 100, 100);
+	doorTo(0, 100, 2000);
+
+	doorTo(60,  95, 500);
+	doorTo(100,  95, 100);
+	armTo(80, 100, 500);
+	armTo(0, 100, 100);
+	doorTo(0, 100, 3000);
+
+	doorTo(60,  95, 500);
+	doorTo(0,   95, 500);
+	doorTo(100,  95, 100);
+	armTo(80, 100, 500);
+	armTo(0, 100, 100);
+	doorTo(0, 100, 2000);
+
+	doorTo(60,  95, 500);
+	doorTo(100,  95, 100);
+	armTo(80, 100, 500);
+	armTo(60, 70, 800);
+	armTo(80, 100, 500);
+	armTo(60, 70, 400);
+	armTo(80, 100, 1500);
+	armTo(100,10,  100);
+	armTo(0, 100, 100);
+	doorTo(0, 100, 2000);
+}
+
+/************************************************/
+void sequence23()
+{
+	doorTo(100, 95, 100);
+	armTo(80, 99,  1000);
+	armTo(85, 1,   900);
+	armTo(90, 1,   800);
+	armTo(95, 1,   300);
+	armTo(100, 1,   10);
+	armTo(0, 100, 100);
+	doorTo(0, 100, 1);
+}
+
+/************************************************/
+void sequence24()
+{
+	doorTo(40, 95, 2000);
+	doorTo(10,  5, 1000);
+	doorTo(50, 95, 2000);
+	doorTo(40, 95, 4000);
+	doorTo(10,  5, 1000);
+	doorTo(60, 95, 2000);
+	doorTo(100,95, 300);
+
+	armTo(70, 20,  10);
+	armTo(90,100,  1);
+	armTo(40, 100, 2000);
+	armTo(50, 100, 1000);
+
+	armTo(70, 20,  10);
+	armTo(90,100,  1);
+	armTo(40, 100, 2000);
+	armTo(50, 100, 1000);
+	armTo(60, 100, 1000);
+
+	armTo(70, 20,  10);
+	armTo(100,100, 250);
+	armTo(0, 100, 100);
+
+	doorTo(0, 100, 1);
+}
+
+/************************************************/
+void sequence25()
+{
+	doorTo(40, 20, 1000);
+
+	doorTo(60,  90, 100);
+	doorTo(80,  90, 100);
+	doorTo(100, 90, 100);
+	doorTo(80, 90, 100);
+	doorTo(60, 90, 100);
+	doorTo(40, 90, 100);
+	doorTo(100, 90, 100);
+
+	armTo(100, 100, 200);
+	armTo(0, 100, 1000);
+
+	doorTo(0, 100, 1);
+}
+
+/************************************************/
+void sequence26()
+{
+
+	doorTo(100, 70, 1000);
+	doorTo(0,   70, 2000);
+
+	doorTo(50,  70, 700);
+	doorTo(100, 60, 800);
+	doorTo(0,   60, 2000);
+
+	doorTo(50,  70, 2500);
+	doorTo(100, 60, 800);
+
+	armTo(80, 100, 100);
+	armTo(100, 100, 200);
+	armTo(0, 100, 1000);
+
+	doorTo(0, 30, 1);
+}
+
+
+/************************************************/
+void sequence27()
+{
+	doorTo(100, 99, 1000);
+
+	armTo(30, 100, 500);
+	armTo(80, 60,  800);
+	armTo(40, 100, 200);
+	armTo(80, 40,  800);
+	armTo(40, 100, 200);
+	armTo(80, 20,  800);
+	armTo(40, 100, 100);
+	armTo(100, 100, 200);
+	armTo(0, 100, 1000);
+	doorTo(0, 100, 1);
+}
+
+
+/************************************************/
+void sequence28()
+{
+	doorTo(40, 99, 1000);
+	doorTo(0,  99, 1000);
+	doorTo(40, 99, 2000);
+	doorTo(0,  99, 1000);
+	doorTo(40, 99, 2500);
+	doorTo(0,  99, 1000);
+	doorTo(40, 99, 2500);
+	doorTo(100, 99, 100);
+	armTo(100, 100, 200);
+	armTo(0, 100, 1000);
+	doorTo(0, 100, 1);
+}
+
+/************************************************/
+void sequence29()
+{
+	doorTo(100, 99, 1000);
+	doorTo(0,   99, 3000);
+	doorTo(100, 99, 100);
+	armTo(100, 100, 200);
+	armTo(0, 100, 100);
+	doorTo(0, 100, 1);
+}
+
+/************************************************/
+void sequence30()
+{
+	doorTo(100, 70, 1000);
+	armTo(80, 100, 2000);
+
+	doorTo(55, 90, 100);
+	doorTo(70, 90, 100);
+	doorTo(55, 90, 100);
+	doorTo(70, 90, 1000);
+	doorTo(100, 70, 2000);
+
+	doorTo(55, 90, 100);
+	doorTo(70, 90, 100);
+	doorTo(55, 90, 100);
+	doorTo(70, 90, 2000);
+	doorTo(100, 70, 3000);
+
+	doorTo(55, 90, 100);
+	doorTo(70, 90, 100);
+	doorTo(55, 90, 100);
+	doorTo(70, 90, 100);
+	doorTo(55, 90, 100);
+	doorTo(70, 90, 100);
+	doorTo(55, 90, 100);
+	doorTo(100, 70, 500);
+
+	armTo(100, 100, 200);
+	armTo(0, 100, 100);
+	doorTo(0, 70, 1);
+}
+
 /************************************************
  * ISR
  ************************************************/
@@ -414,7 +758,23 @@ void (*sequenceFPtr[])(void) =
 	sequence11,
 	sequence12,
 	sequence13,
-	sequence14
+	sequence14,
+	sequence15,
+	sequence16,
+	sequence17,
+	sequence18,
+	sequence19,
+	sequence20,
+	sequence21,
+	sequence22,
+	sequence23,
+	sequence24,
+	sequence25,
+	sequence26,
+	sequence27,
+	sequence28,
+	sequence29,
+	sequence30
 };
 
 /************************************************
@@ -433,11 +793,11 @@ void loop()
 
 	digitalWrite(LED_BUILTIN, HIGH);
 
-	/* Random Sequence */
 	readyServos();
 
-	//  sequence14();
+//	sequence30();
 	
+	/* Random Sequence */
 	int sequenceId = random(0, sizeof(sequenceFPtr) / sizeof(sequenceFPtr[0]));
 	Serial.print("SID: "); Serial.println(sequenceId);
 	sequenceFPtr[sequenceId]();
